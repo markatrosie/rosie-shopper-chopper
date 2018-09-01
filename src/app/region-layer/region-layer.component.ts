b7fcfd92d8769cfc4f0c04634708a40dc7662e4f
Mark Slade <MarkAndrewSlade@gmail.com>
2018-09-01 19:52:27 -0400

    Hooked selected promo into page view.

--- src/app/region-layer/region-layer.component.ts
+++ src/app/region-layer/region-layer.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { fabric } from 'fabric';
+
+import { Promo } from '../promo';
 
 @Component({
   selector: 'app-region-layer',
   templateUrl: './region-layer.component.html',
   styleUrls: ['./region-layer.component.css']
 })
-export class RegionLayerComponent implements OnInit {
+export class RegionLayerComponent implements OnInit, OnChanges {
+  @Input() allPromos: Promo[];
+  @Input() activePromo: Promo;
+
   @ViewChild('canvas') canvasEl: ElementRef;
 
   fabric: fabric.Canvas;
 
   constructor() {}
 
+  ngOnChanges() {
+    console.log('My promos', this.allPromos);
+    console.log('My promo', this.activePromo);
+  }
+
   ngOnInit() {
+
     this.fabric = new fabric.Canvas(this.canvasEl.nativeElement);
   }
 }
